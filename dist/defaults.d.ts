d116e2fd5e1137288a77597e7677efe4d584e014
Sophea Phy <sophea@web-essentials.co>
2021-04-08 14:27:03 +0700

    [TASK] Update ignore list

--- dist/defaults.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as Type from './types';
-export declare const Loader: () => React.ReactElement;
-export declare const Props: Type.Props;
-export declare const ContainerStyle: React.CSSProperties;
-export declare const FrameStyle: (loading: boolean) => React.CSSProperties;
